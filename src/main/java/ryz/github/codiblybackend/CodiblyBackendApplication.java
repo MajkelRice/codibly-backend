2890576229f8c14ac6b9f2171cfb442c3f56164f
Michał Ryz <michalryz2003@gmail.com>
2026-01-09 15:03:13 +0100

    init commit

---
+++ src/main/java/ryz/github/codiblybackend/CodiblyBackendApplication.java
@@ -0,0 +1,13 @@
+package ryz.github.codiblybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiblyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodiblyBackendApplication.class, args);
+    }
+
+}
