34aa32a7496747293fe4b940ef9766fa1104fc1f
Michał Ryz <michalryz2003@gmail.com>
2026-01-12 14:16:51 +0100

    fixed formating for time

--- src/main/java/ryz/github/codiblybackend/client/CarbonIntensityClient.java
+++ src/main/java/ryz/github/codiblybackend/client/CarbonIntensityClient.java
@@ -5,28 +5,36 @@ import org.springframework.web.client.RestClient;
 import ryz.github.codiblybackend.model.CarbonIntensityResponse;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.net.URI;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 @Component
 public class CarbonIntensityClient {
     private final RestClient restClient;
-    private static final String API_URL = "https://api.carbonintensity.org.uk/generation";
+    private static final String API_URL = "https://api.carbonintensity.org.uk";
 
-    public CarbonIntensityClient() {
-        this.restClient = RestClient.builder()
+    private static final DateTimeFormatter API_DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd'T'HH:mm'Z'")
+            .withZone(ZoneId.of("UTC"));
+
+    public CarbonIntensityClient(RestClient.Builder builder) {
+        this.restClient = builder
                 .baseUrl(API_URL)
                 .build();
     }
 
     public CarbonIntensityResponse getGeneration(Instant from, Instant to) {
-        String fromIso = DateTimeFormatter.ISO_INSTANT.format(from.truncatedTo(ChronoUnit.MINUTES));
-        String toIso = DateTimeFormatter.ISO_INSTANT.format(to.truncatedTo(ChronoUnit.MINUTES));
+        String fromIso = API_DATE_FORMATTER.format(from.truncatedTo(ChronoUnit.HOURS));
+        String toIso = API_DATE_FORMATTER.format(to.truncatedTo(ChronoUnit.HOURS));
+
+        String urlString = String.format("/generation/%s/%s", fromIso, toIso);
+
+        System.out.println("Force calling URI: " + API_URL + urlString);
 
         return restClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/from/{from}/to/{to}")
-                        .build(fromIso, toIso))
+                .uri(URI.create(API_URL + urlString))
                 .retrieve()
                 .body(CarbonIntensityResponse.class);
     }
